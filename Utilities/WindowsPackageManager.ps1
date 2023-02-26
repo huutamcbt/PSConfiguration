1ad2e018d1a8d43142155595ee5d04cc16ef4779
huutamcbt <huutamcbt@gmail.com>
2023-02-26 18:37:58 +0700

    Add winget install and uninstall function

--- Utilities/WindowsPackageManager.ps1
+++ Utilities/WindowsPackageManager.ps1
@@ -1,18 +1,18 @@
 # This class is used to manage the method related to WPM actions such as install, uninstall, search,...
-class WindowsPackageManager{
+class WindowsPackageManager {
+    hidden [string]$currentDir =  (Split-Path -Parent $MyInvocation.MyCommand.Definition)
     # Install method is responsible for package installing 
     static [void] Install([string]$IOpt, [string]$Package) {
         try {
             
-            If($IOpt -eq "--name"){
+            If ($IOpt -eq "--name") {
                 switch ($Package) {
-                    "scoop" 
-                    {  
-                        If((Get-Command scoop.ps1 -ErrorAction SilentlyContinue) -or (Get-Command scoop.cmd -ErrorAction SilentlyContinue)){
+                    "scoop" {  
+                        If ((Get-Command scoop.ps1 -ErrorAction SilentlyContinue) -or (Get-Command scoop.cmd -ErrorAction SilentlyContinue)) {
                             Write-Host "The scoop application is already installed"
-                        #    return true 
+                            #    return true 
                         }
-                        Else{
+                        Else {
                             try {
                                 Write-Host "The scoop is installing ..."
                                 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser;
@@ -24,16 +24,72 @@ class WindowsPackageManager{
                             }
                         }
                     }
-                    "chocolatey"
-                    {
+                    "chocolatey" {
+                        If ((Get-Command choco.exe -ErrorAction SilentlyContinue) -or (Get-Command chocolatey.exe -ErrorAction SilentlyContinue)) {
+                            Write-Host "The chocolatey application is already installed"
+                        }
+                        Else {
+                            try {
+                                Write-Host "The chocolatey is installing ..."
+                                # Set directory for installation - Chocolatey does not lock
+                                # down the directory if not the default
+                                $InstallDir = 'C:\ProgramData\chocoportable'
+                                $env:ChocolateyInstall = "$InstallDir"
 
+                                # If your PowerShell Execution policy is restrictive, you may
+                                # not be able to get around that. Try setting your session to
+                                # Bypass.
+                                Set-ExecutionPolicy Bypass -Scope Process -Force;
+
+                                # All install options - offline, proxy, etc at
+                                # https://chocolatey.org/install
+                                Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) | Out-Host
+                            }
+                            catch {
+                                Write-Host "Oops, something went wrong!"
+                            }
+                        }
+                    }
+                    "winget" {
+                        If(Get-Command winget.exe -ErrorAction SilentlyContinue){
+                            Write-Host "The winget application is already installed"
+                        }
+                        Else{
+                            try {
+                                Write-Host "The winget is installing ..."
+                                powershell.exe Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.4.10173/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" `
+                                -OutFile "~/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Method Get| Out-Host
+                                Start-Process -FilePath ~/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle |Out-Host
+                                Remove-Item -Force ~/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+                            }
+                            catch {
+                                Write-Host "Oops, something went wrong!"
+                            }
+                        }
+                    }
+                    "oh-my-posh" {
+                        If(Get-Command oh-my-posh.exe -ErrorAction SilentlyContinue){
+                            Write-Host "The oh-my-posh application is already installed"
+                        }
+                        Else{
+                            try {
+                                Write-Host "The oh-my-posh is installing ..."
+                                Set-ExecutionPolicy Bypass -Scope Process -Force; 
+                                Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1')) | Out-Host
+                            }
+                            catch {
+                                Write-Host "Oops, something went wrong!"
+                            }
+                        }
                     }
                     Default {}
                 }
-            }Elseif(($IOpt -eq "--help") -or($IOpt -eq "-?")){
+            }
+            Elseif (($IOpt -eq "--help") -or ($IOpt -eq "-?")) {
                 [WindowsPackageManager]::ShowInstallInstruction()
                
-            }Else{
+            }
+            Else {
                 [WindowsPackageManager]::ShowLackInstallInstruction()    
             }
         }
@@ -43,16 +99,15 @@ class WindowsPackageManager{
     }
 
     # Uninstall method is responsible for package uninstalling
-    static [void] Uninstall([string]$IOpt, [string]$Package){
+    static [void] Uninstall([string]$IOpt, [string]$Package) {
         try {
-            If($IOpt -eq "--name"){
+            If ($IOpt -eq "--name") {
                 switch ($Package) {
-                    "scoop" 
-                    {  
-                        If((-Not (Get-Command scoop.ps1 -ErrorAction SilentlyContinue)) -and  (-Not (Get-Command scoop.cmd -ErrorAction SilentlyContinue))){
+                    "scoop" {  
+                        If ((-Not (Get-Command scoop.ps1 -ErrorAction SilentlyContinue)) -and (-Not (Get-Command scoop.cmd -ErrorAction SilentlyContinue))) {
                             Write-Host "The scoop application is already uninstalled"
                         }
-                        Else{
+                        Else {
                             try {
                                 Write-Host "The scoop is uninstalling ..."
                                 Remove-Item -Force -Recurse ~/scoop
@@ -63,9 +118,51 @@ class WindowsPackageManager{
                             }
                         }
                     }
-                    "chocolatey"
-                    {
-
+                    "chocolatey" {
+                        If ((-Not (Get-Command choco.exe -ErrorAction SilentlyContinue)) -and (-Not (Get-Command chocolatey.exe -ErrorAction SilentlyContinue))) {
+                            Write-Host "The chocolatey application is already uninstalled"
+                        }
+                        Else {
+                            try {
+                                Write-Host "The chocolatey is uninstalling ..."
+                                Remove-Item -Force -Recurse $env:ChocolateyInstall
+                                Remove-Item -Force -Recurse 'C:\ProgramData\chocoportable'
+                                Write-Host "The chocolatey is uninstalled"
+                            }
+                            catch {
+                                Write-Host "Oops, something went wrong!"
+                            }
+                        }
+                    }
+                    "winget" {
+                        If(-Not (Get-Command winget.exe -ErrorAction SilentlyContinue)){
+                            Write-Host "The winget application is already uninstalled"
+                        }
+                        Else{
+                            try {
+                                Write-Host "The winget is uninstalling ..."
+                                Remove-AppPackage (Get-AppPackage -Name Microsoft.DesktopAppInstaller).PackageFullName | Out-Host
+                                Write-Host "The winget is uninstalled"
+                            }
+                            catch {
+                                Write-Host "Oops, something went wrong!"
+                            }
+                        }
+                    }
+                    "oh-my-posh" {
+                        If(-Not (Get-Command oh-my-posh.exe -ErrorAction SilentlyContinue)){
+                            Write-Host "The oh-my-posh application is already uninstalled"
+                        }
+                        Else{
+                            try {
+                                Write-Host "The oh-my-posh is uninstalling ..."
+                                Remove-Item -Recurse -Force "C:\Users\vboxuser\AppData\Local\Programs\oh-my-posh\"
+                                Write-Host "The oh-my-posh is uninstalled"
+                            }
+                            catch {
+                                Write-Host "Oops, something went wrong!"
+                            }
+                        }
                     }
                     Default {}
                 }
@@ -76,6 +173,10 @@ class WindowsPackageManager{
         }
     }
     
+    static [void] List([string]$IOpt, [string]$Package){
+        
+    }
+
     # Show lack installing instruction
     static [void] ShowLackInstallInstruction() {
         Write-Host "Customizable Windows Package Manager v1.0 `n"
@@ -117,11 +218,11 @@ class WindowsPackageManager{
         Write-Host "More help can be found at : https://archlinux.org/"
     }
     
-    static [void] ShowVersion(){
+    static [void] ShowVersion() {
         Write-Host "v1.0"
     }
 
-    static [void] ShowInfo(){
+    static [void] ShowInfo() {
         Write-Host "Customizable Windows Package Manager v1.0"
         Write-Host "Copyright (c) huutamcbt `n"
         Write-Host "Windows: Windows.Desktop"
