e0328e4e4dd2f2554e1910e09059d19deab39abd
huutamcbt <huutamcbt@gmail.com>
2023-03-09 12:10:02 +0700

    Change the Get-Size function

--- Utilities/GetSize.ps1
+++ Utilities/GetSize.ps1
@@ -6,42 +6,42 @@ class GetSize {
                     Write-Host $Path;
                     Write-Host "Size:`t" -ForegroundColor Green -NoNewline;
                     Write-Host "$((Get-ChildItem  $Path -Recurse -Force| Measure-Object -Property Length -Sum).sum)" -NoNewline;
-                    Write-Host "B" -ForegroundColor Red
+                    Write-Host "B" -ForegroundColor Red;
                 }
                 "kb" {
                     Write-Host "Name:`t" -ForegroundColor Green -NoNewline; 
                     Write-Host $Path;
                     Write-Host "Size:`t" -ForegroundColor Green -NoNewline;
                     Write-Host "$((Get-ChildItem  $Path -Recurse -Force| Measure-Object -Property Length -Sum).sum / 1KB)" -NoNewline;
-                    Write-Host "KB" -ForegroundColor Red
+                    Write-Host "KB" -ForegroundColor Red;
                 }
                 "mb" {
                     Write-Host "Name:`t" -ForegroundColor Green -NoNewline; 
                     Write-Host $Path;
                     Write-Host "Size:`t" -ForegroundColor Green -NoNewline;
                     Write-Host "$((Get-ChildItem  $Path  -Force -Recurse| Measure-Object -Property Length -Sum).sum / 1MB)" -NoNewline;
-                    Write-Host "MB" -ForegroundColor Red
+                    Write-Host "MB" -ForegroundColor Red;
                 }
                 "gb" {
                     Write-Host "Name:`t" -ForegroundColor Green -NoNewline; 
                     Write-Host $Path;
                     Write-Host "Size:`t" -ForegroundColor Green -NoNewline;
                     Write-Host "$((Get-ChildItem  $Path -Force -Recurse| Measure-Object -Property Length -Sum).sum / 1GB)" -NoNewline;
-                    Write-Host "GB" -ForegroundColor Red
+                    Write-Host "GB" -ForegroundColor Red;
                 }
                 "tb" {
                     Write-Host "Name:`t" -ForegroundColor Green -NoNewline; 
                     Write-Host $Path;
                     Write-Host "Size:`t" -ForegroundColor Green -NoNewline;
                     Write-Host "$((Get-ChildItem  $Path -Force -Recurse| Measure-Object -Property Length -Sum).sum / 1TB)" -NoNewline;
-                    Write-Host "TB" -ForegroundColor Red
+                    Write-Host "TB" -ForegroundColor Red;
                 }
                 "pb" {
                     Write-Host "Name:`t" -ForegroundColor Green -NoNewline; 
                     Write-Host $Path;
                     Write-Host "Size:`t" -ForegroundColor Green -NoNewline;
                     Write-Host "$((Get-ChildItem  $Path -Recurse -Force| Measure-Object -Property Length -Sum).sum / 1PB)" -NoNewline;
-                    Write-Host "PB" -ForegroundColor Red
+                    Write-Host "PB" -ForegroundColor Red;
                 }
                 Default {}
             }
